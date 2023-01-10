cffb3494b7829664462fcc547f634d08338e7bed
Kunal <kunal3270@gmail.com>
2023-01-10 13:42:34 +0530

    updated

--- 2234542_Simple_cucumber_project_with_phptravels/src/test/java/allTest/CucumberRunner.java
+++ 2234542_Simple_cucumber_project_with_phptravels/src/test/java/allTest/CucumberRunner.java
@@ -4,7 +4,7 @@ import baseClass.BaseClass;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "", features = {"src/test/resources/features/SignUp.feature"},
-				 glue = {"allTest"}, plugin = {"pretty"}, dryRun = false)
+				 glue = {"allTest"}, plugin = {"pretty"}, monochrome = true)
 public class CucumberRunner extends BaseClass
 {
 
